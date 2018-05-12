23f292586af4aacf54bd52f45548647cb50df940
Simaelo <elo234@gmail.com>
2018-05-12 07:00:08 -0400

    Second

---
+++ build-ListTree-Desktop-Default/CMakeFiles/ListTree.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/Day5/ListTree/list.cpp" "/home/user/Day5/build-ListTree-Desktop-Default/CMakeFiles/ListTree.dir/list.cpp.o"
+  "/home/user/Day5/ListTree/main.cpp" "/home/user/Day5/build-ListTree-Desktop-Default/CMakeFiles/ListTree.dir/main.cpp.o"
+  "/home/user/Day5/ListTree/tree.cpp" "/home/user/Day5/build-ListTree-Desktop-Default/CMakeFiles/ListTree.dir/tree.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-ListTree-Desktop-Default/CMakeFiles/ListTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ListTree.dir/list.cpp.o"
+  "CMakeFiles/ListTree.dir/main.cpp.o"
+  "CMakeFiles/ListTree.dir/tree.cpp.o"
+  "ListTree.pdb"
+  "ListTree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ListTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
